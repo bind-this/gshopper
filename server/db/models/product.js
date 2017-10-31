f90f2f09a1c11b502222184912b0d9c5cfe0a0c2
Mike Romani <mr4227@gmail.com>
2017-10-31 11:32:45 -0400

    feat(product model): make product model

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -2,7 +2,38 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 
 const Product = db.define('product', {
-  name: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    get() {
+      const price = this.getDataValue('price')
+      return this.getDataValue('price', '$' + price)
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  },
+  img: {
+    type: Sequelize.STRING,
+    defaultValue: 'product-placeholder-image.jpg',
+    isUrl: true
+  },
+  availability: {
+    type: Selection.BOOLEAN,
+    defaultValue: false
+  }
 });
 
 module.exports = Product;
