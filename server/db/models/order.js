b55d4689cc2729ae553f2b3b733c9f7894e9978c
Kyle Uehlein <kyleuehlein@gmail.com>
2017-10-31 19:46:08 -0400

    feat(order-model): make default value "created"

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -4,6 +4,7 @@ const db = require('../db')
 const Order = db.define('order', {
   status: {
     type: Sequelize.ENUM('created', 'processing', 'completed', 'cancelled'), //created means its a 'cart', processing begins when cart is submitted
+    defaultValue: 'created',
     allowNull: false
   },
   sessionId: {
