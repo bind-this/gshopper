8b6b0521911c09ea9bc09f240829493f465ab8a9
Mike Romani <mr4227@gmail.com>
2017-10-31 18:26:53 -0400

    feat(category-route) make category routes

---
+++ server/api/categories.js
@@ -0,0 +1,55 @@
+const router = require("express").Router();
+const { Category } = require("../db/models");
+
+//Set variable to category instance
+router.param("id", (req, res, next, id) => {
+  Category.findOne({
+    where: { id: id },
+    include: [{ all: true }]
+  })
+    .then(category => {
+      if (!category) {
+        const err = Error("category not found");
+        err.status = 404;
+        next(err);
+      } else {
+        req.category = category;
+        next();
+      }
+    })
+    .catch(next);
+});
+
+//get all categories
+router.get("/", (req, res, next) => {
+  Category.findAll()
+    .then(categories => res.status(200).json(categories))
+    .catch(next);
+});
+
+//create new category
+router.post("/", (req, res, next) => {
+  Category.create(req.body)
+    .then(category => res.json(category))
+    .catch(next);
+});
+
+//get one category (by id) - (and eager load associated products - see router.param)
+router.get("/:id", (req, res, next) => {
+  res.json(req.category).catch(next);
+});
+
+//deletes one category (by id)
+router.delete("/:id", (req, res, next) => {
+  req.category
+    .destroy()
+    .then(deleted => res.sendStatus(204))
+    .catch(next);
+});
+
+//updates one category (by id)
+router.put("/:id", (req, res, next) => {
+  Category.update(req.body)
+    .then(category => res.json(category))
+    .catch(next);
+});
--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/orders', require('./orders'))
 router.use('/order-products', require('./order_products'))
+router.use('/categories', require('./catorgies'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
