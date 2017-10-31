8f62b9ae0f302d3b1111e070cb2a887b58f09ff4
Mike Romani <mr4227@gmail.com>
2017-10-31 18:35:02 -0400

    feat(category-routes) make routes

--- server/api/index.js
+++ server/api/index.js
@@ -6,7 +6,7 @@ router.use('/products', require('./products'))
 
 router.use('/orders', require('./orders'))
 router.use('/order-products', require('./order_products'))
-router.use('/categories', require('./catorgies'))
+router.use('/categories', require('./categories'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
