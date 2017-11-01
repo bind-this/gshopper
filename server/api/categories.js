186407275669c55832da4171f70ab875facdcb5e
Mike Romani <mr4227@gmail.com>
2017-11-01 19:46:17 -0400

    feat(cat-route-test) test get by id route, clear up various comments

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const { Category } = require("../db/models");
 
-//Set variable to category instance
+//PARAM - finds category by id, sets instance to req.category
 router.param("id", (req, res, next, id) => {
   Category.findById(id, { include: [{ all: true }] })
     .then(category => {
@@ -17,26 +17,26 @@ router.param("id", (req, res, next, id) => {
     .catch(next);
 });
 
-//get all categories
+//GET - gets all categories
 router.get("/", (req, res, next) => {
   Category.findAll()
     .then(categories => res.status(200).json(categories))
     .catch(next);
 });
 
-//create new category
+//Post - creates new category
 router.post("/", (req, res, next) => {
   Category.create(req.body)
     .then(category => res.json(category))
     .catch(next);
 });
 
-//get one category (by id) - (and eager load associated products - see router.param)
+//GET - gets category by id, eager loads all products
 router.get("/:id", (req, res, next) => {
   res.json(req.category);
 });
 
-//deletes one category (by id)
+//DELETE - deletes category by id
 router.delete("/:id", (req, res, next) => {
   req.category
     .destroy()
@@ -44,10 +44,9 @@ router.delete("/:id", (req, res, next) => {
     .catch(next);
 });
 
-//updates one category (by id)
+//PUT - updates category by id
 router.put("/:id", (req, res, next) => {
-  req.category
-    .update(req.body)
+  req.category.update(req.body)
     .then(category => res.json(category))
     .catch(next);
 });
