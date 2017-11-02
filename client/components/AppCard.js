78254555f96ae586ebd74bf92f8d1bf2a2b205bd
Adrien Lacquemant <verblodung@gmail.com>
2017-11-02 14:36:10 -0400

    adding links to pictures

--- client/components/AppCard.js
+++ client/components/AppCard.js
@@ -1,29 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import { Card, Icon, Image, Rating } from 'semantic-ui-react'
 
 const AppCard = (props) => (
   <Card raised>
-    <Image height="290px" src={props.product.img} />
-    <Card.Content>
-      <Card.Header>
-        {props.product.name}
-      </Card.Header>
-      <Card.Meta>
-        <span className='card-category'>
-          {props.product.categories[0].name} <Rating defaultRating={3} maxRating={5} disabled />
-        </span>
-      </Card.Meta>
-      <Card.Description>
-        { props.product.description.slice(0, 70) }...
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        ${ props.product.price / 100 }
-      </a>
-    </Card.Content>
-  </Card>
+      <NavLink to={`/products/${props.product.id}`}>
+        <Image height="290px" src={props.product.img} />
+      </NavLink>
+      <Card.Content>
+        <Card.Header>
+          {props.product.name}
+        </Card.Header>
+        <Card.Meta>
+          <span className='card-category'>
+            {props.product.categories[0].name} <Rating defaultRating={3} maxRating={5} disabled />
+          </span>
+        </Card.Meta>
+        <Card.Description>
+          { props.product.description.slice(0, 70) }...
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+          <Icon name='user' />
+          ${ props.product.price / 100 }
+      </Card.Content>
+    </Card>
 )
 
 export default AppCard
