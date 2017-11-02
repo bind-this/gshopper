81286a3dd18f20ac61c720c146d92ff6edce383d
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-02 12:42:26 -0400

    feat(navbar): add basic navbar

--- client/components/main.js
+++ client/components/main.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import { logout } from '../store'
 
+import { Input, Menu } from 'semantic-ui-react'
 /**
  * COMPONENT
  *  The Main component is our 'picture frame' - it displays the navbar and anything
  *  else common to our entire app. The 'picture' inside the frame is the space
  *  rendered out by the component's `children`.
  */
-const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+const Main = props => {
+  const { children, handleClick, isLoggedIn } = props
 
   return (
     <div>
-      <h1>BOILERMAKER</h1>
-      <nav>
-        {
-          isLoggedIn
-            ? <div>
-              {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
-              <a href="#" onClick={handleClick}>Logout</a>
-            </div>
-            : <div>
-              {/* The navbar will show these links before you log in */}
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Sign Up</Link>
-            </div>
-        }
-      </nav>
+      <Menu size="big">
+        <Menu.Item name="Logo goes here" href="/" />
+        <Menu.Item>
+          <Input placeholder="Search..." />
+        </Menu.Item>
+        {isLoggedIn ? (
+          <Menu.Menu position="right">
+            {/* The navbar will show these links after you log in */}
+            <Menu.Item name="Cart" href="/cart" />
+            <Menu.Item name="Home" href="/home" />
+            <Menu.Item name="Logout" onClick={handleClick} />
+          </Menu.Menu>
+        ) : (
+          <Menu.Menu position="right">
+            {/* The navbar will show these links before you log in */}
+            <Menu.Item name="Cart" href="/cart" />
+            <Menu.Item name="Login" href="/login" />
+            <Menu.Item name="Sign Up" href="/signup" />
+          </Menu.Menu>
+        )}
+      </Menu>
+      <Menu size="mini">
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+        <Menu.Item name="category" href="/category" />
+      </Menu>
       <hr />
       {children}
     </div>
@@ -40,15 +58,15 @@ const Main = (props) => {
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
   }
 }
 
-const mapDispatch = (dispatch) => {
+const mapDispatch = dispatch => {
   return {
-    handleClick () {
+    handleClick() {
       dispatch(logout())
     }
   }
