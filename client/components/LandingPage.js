fb1dd9af3c9e4f282e8c30464fb333d8ff9e18da
Alejandro Villarreal <avillarreal01@gmail.com>
2017-11-04 14:42:24 -0400

    feat(home): add carousel component

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Divider } from 'semantic-ui-react'
 
+import Carousel from 'nuka-carousel'
+
 function LandingPage(props) {
   const products25 = props.products.slice(0, 25)
   return (
@@ -10,6 +12,14 @@ function LandingPage(props) {
         <Divider horizontal>Hello, {props.user.firstName}</Divider>
       )}
       <div>Welcome home!</div>
+      <Carousel slidesToShow={3} cellAlign="center">
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide1" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide2" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide3" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide4" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide5" />
+        <img src="http://placehold.it/1000x400/ffffff/c0392b/&text=slide6" />
+      </Carousel>
       <ul>{products25.map(product => <li>{product.name}</li>)}</ul>
     </div>
   )
