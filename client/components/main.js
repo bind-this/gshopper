d1dc4993672f221a565af760fe29563df50a1f61
Adrien <verblodung@gmail.com>
2017-11-04 21:22:28 -0400

    pulling the cart in the navbar

--- client/components/main.js
+++ client/components/main.js
@@ -35,7 +35,7 @@ class Main extends Component {
             </Menu.Item>
             <Menu.Item name="Cart" onClick={() => history.push('/cart')}>
               <Icon size="big" name="shop" />
-              <Label color="teal">99+</Label>
+              <Label color="teal">{user.id && user.orders.filter(order => order.status === 'created')[0].order_products.length}</Label>
             </Menu.Item>
             {isLoggedIn ? (
               <UserDropdown user={user} tryLogout={tryLogout} />
