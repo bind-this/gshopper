041f40d0cc9727683b45cbe6668cebca3bb81a9b
Kyle Uehlein <kyleuehlein@gmail.com>
2017-11-07 16:08:05 -0500

    feat(review): add review form conditionally renders

--- client/components/Single-Product/AddReview.js
+++ client/components/Single-Product/AddReview.js
@@ -1,36 +1,38 @@
-'use strict'
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { makeReview, fetchProduct } from '../../store'
+'use strict';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { makeReview, fetchProduct } from '../../store';
 
 class AddReview extends Component {
   constructor(props) {
-    super(props)
-    this.tempReview = {}
-    this.handleCommentChange = this.handleCommentChange.bind(this)
-    this.handleRatingChange = this.handleRatingChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    super(props);
+    this.tempReview = {};
+    this.handleCommentChange = this.handleCommentChange.bind(this);
+    this.handleRatingChange = this.handleRatingChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleCommentChange(event) {
-    this.tempReview[event.target.name] = event.target.value
+    this.tempReview[event.target.name] = event.target.value;
   }
 
   handleRatingChange(event) {
-    this.tempReview.userId = this.props.user.id
-    this.tempReview.productId = this.props.product.id
-    this.tempReview[event.target.name] = event.target.value
+    this.tempReview.userId = this.props.user.id;
+    this.tempReview.productId = this.props.product.id;
+    this.tempReview[event.target.name] = event.target.value;
   }
 
   handleSubmit(event) {
-    event.preventDefault()
-    this.props.makeReview(this.tempReview, this.props.product.id)
-    this.props.fetchProduct(this.props.product.id)
+    event.preventDefault();
+    this.props.makeReview(this.tempReview, this.props.product.id);
+    this.props.fetchProduct(this.props.product.id);
+    this.tempReview = {
+      comment: '',
+      rating: 0
+    }
   }
 
   render() {
-    //let user = this.props.user
-    /* <div>{ user.orders && user.orders.some(order => order.order_products.map(obj => obj.prod.id).join(', ')  // .includes(this.props.product.id)) }</div> */
     return (
       <div>
         <form onSubmit={this.handleSubmit} className="ui reply form">
@@ -39,12 +41,14 @@ class AddReview extends Component {
             type="text"
             placeholder="submit review here"
             className="field"
+            value={ this.tempReview.comment }
             name="comment"
             onChange={this.handleCommentChange}
           />
           <select
             className="field"
             required="required"
+            value={ this.tempReview.rating }
             name="rating"
             onChange={this.handleRatingChange}
           >
@@ -64,7 +68,7 @@ class AddReview extends Component {
           </button>
         </form>
       </div>
-    )
+    );
   }
 }
 
@@ -72,11 +76,11 @@ const mapStateToProps = state => ({
   review: state.review,
   user: state.user,
   product: state.product
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   makeReview: (review, productId) => dispatch(makeReview(review, productId)),
   fetchProduct: id => dispatch(fetchProduct(id))
-})
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddReview)
+export default connect(mapStateToProps, mapDispatchToProps)(AddReview);
--- client/components/Single-Product/Review.js
+++ client/components/Single-Product/Review.js
@@ -1,17 +1,35 @@
-'use strict'
+'use strict';
 
-import React from 'react'
-import MapReview from './MapReview'
-import AddReview from './AddReview'
+import React from 'react';
+import { connect } from 'react-redux';
+import MapReview from './MapReview';
+import AddReview from './AddReview';
 
-const review = ({ reviews }) => (
-  <div style={{ display: 'flex', justifyContent: 'center', padding: '1em' }}>
-    <div className="ui comments">
-      <h3 className="ui dividing header">Customer Reviews</h3>
-      <MapReview reviews={reviews} />
-      <AddReview />
+const Review = (props) => {
+  let user = props.user;
+  let visable = false;
+  visable =
+    user &&
+    user.orders &&
+    user.orders[0].order_products &&
+    user.orders.some(order =>
+      order.order_products.some(obj => obj.product.id === props.product.id)
+    );
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '1em' }}>
+      <div className="ui comments">
+        <h3 className="ui dividing header">Customer Reviews</h3>
+        <MapReview reviews={props.reviews} />
+        {visable ? <AddReview /> : ''}
+      </div>
     </div>
-  </div>
-)
+  );
+};
 
-export default review
+const mapStateToProps = state => ({
+  user: state.user,
+  product: state.product
+});
+
+export default connect(mapStateToProps)(Review);
