55abaa06f145955e0b530c4f4fe0ca509a3c7f0e
Doug Chin <dougmchin@gmail.com>
2017-11-07 16:28:11 -0500

    f(testbranch) still testing

--- server/db/models/user-model.spec.js
+++ server/db/models/user-model.spec.js
@@ -1,12 +1,41 @@
-/* global describe beforeEach it */
+// four tests for user model
 
-const {expect} = require('chai')
+const { expect, assert } = require('chai')
 const db = require('../index')
 const User = db.model('user')
 
 describe('User model', () => {
   beforeEach(() => {
     return db.sync({force: true})
+  })
+
+  describe('email and password fields', () => {
+    let testUser
+    User.create({
+      email: 'uniqueAddress@mail.test',
+      password: 'testpw'
+    })
+    .then(user => {
+      testUser = user
+    })
+
+    it('email is correct and unique', () => {
+      expect(testUser.email).to.be.equal('uniqueAddress@mail.test')
+    })
+
+    it('password is a string in db', () => {
+      expect(testUser.password).to.be.a('string')
+    })
+
+    it('password is salted in db', () => {
+      expect(testUser.password).to.be.not.equal('testpw')
+    })
+  })
+
+  describe('associations', () => {
+    it('should have two associations', () => {
+      expect(Object.keys(User.associations)).to.deep.equal(['orders', 'reviews'])
+    })
   })
 
   describe('instanceMethods', () => {
