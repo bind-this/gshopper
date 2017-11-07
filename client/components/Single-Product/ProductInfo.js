8190583339e40c479980430e4888891a451cb497
Mike Romani <mr4227@gmail.com>
2017-11-07 13:58:17 -0500

    feat(prod-page) adjusted styling to semantic ui

--- client/components/Single-Product/ProductInfo.js
+++ client/components/Single-Product/ProductInfo.js
@@ -1,17 +1,21 @@
-'use strict';
+'use strict'
 
-import React from 'react';
+import React from 'react'
 
 const ProductInfo = ({ product }) => (
   <div style={{ display: 'flex', flexDirection: 'column', margin: '1.5em' }}>
-    <div>{product.author}</div>
-    <div>
-      { product.categories && product.categories.map(cat => cat.name).join(', ') }
-    </div>
-    <div style={{ margin: '1.5em', flexGrow: '2', display: 'flex', justifyContent: 'center' }}>
+    <div>Product Description:</div>
+    <div
+      style={{
+        margin: '1.5em',
+        flexGrow: '2',
+        display: 'flex',
+        justifyContent: 'center'
+      }}
+    >
       <div>{product.description}</div>
     </div>
   </div>
-);
+)
 
-export default ProductInfo;
+export default ProductInfo
